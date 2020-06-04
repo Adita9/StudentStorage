8832574c40917e87f2451564ddbf186d811a270b
aneagoe <adrian.neagoe@1and1.ro>
2020-06-05 00:44:42 +0300

    added the documents to the repo

--- Examen_S4_Ro/StudentStorage/src/main/java/studentstorage/persistence/StudentRepository.java
+++ Examen_S4_Ro/StudentStorage/src/main/java/studentstorage/persistence/StudentRepository.java
@@ -2,7 +2,9 @@ package studentstorage.persistence;
 
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import studentstorage.entity.Student;
 
+@Repository
 public interface StudentRepository extends CrudRepository<Student, Integer> {
 }
